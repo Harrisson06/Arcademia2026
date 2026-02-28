da0362091b33141c9d9e1f5f9db976828425bea5
dev-cyw <98186075+dev-cyw@users.noreply.github.com>
2026-02-28 15:13:06 +0000

    basic inventory system

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class CollectableItem : Interactable
 {
+    public ItemData itemData;
+
     protected override void OnInteract()
     {
         InteractablePromptUI.Instance?.HidePrompt();
-        print("COLLECTED");
+        Inventory.Instance.AddItem(itemData);
+        
+        if (itemData.collectSound != null)
+            AudioSource.PlayClipAtPoint(itemData.collectSound, (Vector3)transform.position);
+
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -11,7 +11,6 @@ public class Shop : MonoBehaviour
         if (PlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
             ShopUI.SetActive(true);
-            print(healthBar);
             healthBar.SetActive(false);
         }
     }
